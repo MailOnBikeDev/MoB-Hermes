288d16ffad94606a9fa4cd1731c04b40c41f3ff2
Dan Colmenares <mailonbikedev@gmail.com>
2021-04-05 13:07:47 -0500

    Añadiendo una columna de comentarios para los Pedidos

--- src/models/pedido.model.js
+++ src/models/pedido.model.js
@@ -103,6 +103,10 @@ module.exports = (sequelize, Sequelize) => {
 				type: Sequelize.STRING(15),
 				allowNull: false,
 			},
+			comentario: {
+				type: Sequelize.STRING(50),
+				allowNull: true,
+			},
 		},
 		{
 			tableName: "pedidos",
