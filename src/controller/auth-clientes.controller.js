72eb20aa76f40c7a23b34569d39a480293ad6f2b
Dan <mailonbikedev@gmail.com>
2021-07-09 15:36:29 -0500

    Ajustando parametros del loginCliente

--- src/controller/auth-clientes.controller.js
+++ src/controller/auth-clientes.controller.js
@@ -1,6 +1,15 @@
 const db = require("../models/index");
 const config = require("../config/auth.config");
 const UserCliente = db.userCliente;
+const Cliente = db.cliente;
+const Distrito = db.distrito;
+const Comprobante = db.comprobante;
+const RolCliente = db.rolCliente;
+const Carga = db.carga;
+const Envio = db.envio;
+const FormaDePago = db.formaDePago;
+
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   // Login
@@ -13,6 +22,30 @@ module.exports = {
       if (!user) {
         return res.status(404).send({ message: "Usuario no encontrado." });
       }
+
+      const clienteAsignado = await Cliente.findOne({
+        where: { id: user.clienteId },
+        include: [
+          {
+            model: Distrito,
+          },
+          {
+            model: Comprobante,
+          },
+          {
+            model: RolCliente,
+          },
+          {
+            model: Carga,
+          },
+          {
+            model: FormaDePago,
+          },
+          {
+            model: Envio,
+          },
+        ],
+      });
 
       // Creando el token
       const token = jwt.sign({ data: user }, config.secret, {
@@ -27,6 +60,7 @@ module.exports = {
         email: user.email,
         accessToken: token,
         message: "Inicio de sesión correcto",
+        clienteAsignado,
       });
     } catch (error) {
       res.status(500).json({ message: error.message });
